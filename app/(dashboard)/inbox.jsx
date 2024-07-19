c73abe33628a0742f3607f5e40c820455ac24056
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-19 17:25:18 +0300

    inbox screen

---
+++ app/(dashboard)/inbox.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyInbox = () => {
+  return (
+    <View>
+      <Text>MyInbox</Text>
+    </View>
+  )
+}
+
+export default MyInbox
