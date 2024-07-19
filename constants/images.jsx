7ab40f62af8b18aff6c2dfc91eb06e59bde04070
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-19 17:11:49 +0300

    images

---
+++ constants/images.jsx
@@ -0,0 +1,7 @@
+import bank from '../assets/bank.jpg'
+import free from '../assets/free.jpg'
+import transfer from '../assets/Money-Transfer.png'
+import pay from '../assets/pay.png'
+import withdraw from '../assets/withdraw.png'
+import profile from '../assets/profile.jpeg'
+export default {bank, free, transfer,pay, withdraw, profile}
