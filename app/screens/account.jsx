acdd802d3f9f90464a815110d627e123c5e3f765
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-29 17:09:52 +0300

    account

---
+++ app/screens/account.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Account = () => {
+  return (
+    <View>
+      <Text className="text-center">Account</Text>
+    </View>
+  )
+}
+
+export default Account
